9cece96504d4c0be3220145ccae49d2612b39151
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-02-16 19:18:21 -0500

    Creates GeometricShape project in NetBeans IDE 11.0

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,52 @@
+/*
+ * Shankar Ghimire
+ * StudentId:991585190
+ * Course: ...............
+ */
+package shapes;
+
+/**
+ *
+ * @author Dell
+ */
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    /**
+     * @return the length
+     */
+    public double getLength() {
+        return length;
+    }
+
+    /**
+     * @param length the length to set
+     */
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    /**
+     * @return the width
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    
+}
