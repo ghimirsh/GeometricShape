87ea05d4c327d0d17c5e6bd714b0e06ad52d2173
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-02-16 19:27:08 -0500

    Adds Square.java class and class diagram of the classes.

---
+++ src/shapes/Square.java
@@ -0,0 +1,33 @@
+package shapes;
+
+public class Square {
+
+	private double length;
+
+	public double getLength() {
+		return this.length;
+	}
+
+	/**
+	 * 
+	 * @param length
+	 */
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public Square() {
+		// TODO - implement Square.Square
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param length
+	 */
+	public Square(int length) {
+		// TODO - implement Square.Square
+		throw new UnsupportedOperationException();
+	}
+
+}
